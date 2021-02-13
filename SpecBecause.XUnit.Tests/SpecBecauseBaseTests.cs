3bb0c75b8a1826f1a876db23620a4f3f1b26a945
Danny Warren <dannydwarren@gmail.com>
2021-02-13 00:48:21 -0700

    Add ctor with default new Engine

--- SpecBecause.XUnit.Tests/SpecBecauseBaseTests.cs
+++ SpecBecause.XUnit.Tests/SpecBecauseBaseTests.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace SpecBecause.XUnit.Tests
 {
-    public class SpecBecauseBaseTests
+    public class SpecBecauseBaseTests : SpecBecauseBase
     {
         [Fact]
         public void When_the_type_SpecBecauseBase_is_loaded()
@@ -18,6 +18,33 @@ namespace SpecBecause.XUnit.Tests
                     x.Name.ShouldBe(nameof(Engine));
                     x.PropertyType.Name.ShouldBe(nameof(IEngine));
                 });
+
+            specBecauseBaseType.GetConstructor(new[] { typeof(IEngine) })
+                .ShouldSatisfyAllConditions(x =>
+                {
+                    x.ShouldNotBeNull();
+                    x.GetParameters().ShouldHaveSingleItem()
+                        .ShouldSatisfyAllConditions(y =>
+                        {
+                            y.Name.ShouldBe("engine");
+                            y.ParameterType.Name.ShouldBe(nameof(IEngine));
+                            y.DefaultValue.ShouldBeNull();
+                        });
+                });
+        }
+
+        [Fact]
+        public void When_constructing_with_defaults()
+        {
+            var classUnderTest = new SpecBecauseBase();
+
+            var engineProperty = typeof(SpecBecauseBase).GetProperty("Engine", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            engineProperty!.GetValue(classUnderTest).ShouldSatisfyAllConditions(x =>
+            {
+                x.ShouldNotBeNull();
+                x.ShouldBeOfType<Engine>();
+            });
         }
     }
 }
--- SpecBecause.XUnit/SpecBecauseBase.cs
+++ SpecBecause.XUnit/SpecBecauseBase.cs
@@ -3,5 +3,10 @@ namespace SpecBecause.XUnit
     public class SpecBecauseBase
     {
         private IEngine Engine { get; }
+
+        public SpecBecauseBase(IEngine? engine = null)
+        {
+            Engine = engine ?? new Engine();
+        }
     }
 }
